555f004e569f17d658fb8a9dc0afc3726c48b481
Jason White <jason.white@shopify.com>
2017-09-03 21:07:36 -0400

    finally working

--- src/bookie/main.py
+++ src/bookie/main.py
@@ -1,7 +1,9 @@
 import json
+import logging
 
 import flask
 import httplib2
+import uuid
 
 from google.cloud import storage
 from apiclient import discovery
@@ -9,6 +11,12 @@ from oauth2client import client
 import tempfile
 
 app = flask.Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+app.config['PREFERRED_URL_SCHEME'] = 'https'
+app.secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
 
 @app.route('/')
@@ -25,9 +33,6 @@ def index():
         return json.dumps(files)
 
 
-SECRETS_FILENAME = 'client_secrets.json'
-
-
 @app.route('/oauth2callback')
 def oauth2callback():
     flow = client.flow_from_clientsecrets(
@@ -41,25 +46,28 @@ def oauth2callback():
         auth_code = flask.request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
         flask.session['credentials'] = credentials.to_json()
-        return flask.redirect(flask.url_for('index'))
+        return flask.redirect(flask.url_for('index', _scheme='https', _external=True))
 
 
-def get_secrets(filename: str) -> None:
-    client = storage.Client()
-    bucket = client.bucket('bookie-178719.appspot.com')
+@app.route('/_ah/health')
+def health_check():
+    return 'ok', 200
+
+
+def get_secrets() -> str:
+    secrets_file = tempfile.NamedTemporaryFile(delete=False)
+    gclient = storage.Client()
+    bucket = gclient.bucket('bookie-178719.appspot.com')
     blob = bucket.blob('secrets/client_secrets.json')
-    blob.download_to_filename(filename)
+    blob.download_to_filename(secrets_file.name)
+    return secrets_file.name
+
+
+SECRETS_FILENAME = get_secrets()
 
 
 if __name__ == '__main__':
-    with tempfile.NamedTemporaryFile() as secrets_file:
-        get_secrets(secrets_file.name)
-        SECRETS_FILENAME = secrets_file.name
-
-        # This is used when running locally. Gunicorn is used to run the
-        # application on Google App Engine. See entrypoint in app.yaml.
-        import uuid
-
-        app.secret_key = str(uuid.uuid4())
-        app.debug = False
-        app.run(host="0.0.0.0", port=8000)
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.debug = False
+    app.run(host="0.0.0.0", port=8000)
