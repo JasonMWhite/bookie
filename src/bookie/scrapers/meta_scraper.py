3d875c5564db86aae09a32dcb4e7ab615b9c0f78
Jason White <jason.white@shopify.com>
2017-09-05 00:56:01 -0400

    add bookscouter

--- src/bookie/scrapers/meta_scraper.py
+++ src/bookie/scrapers/meta_scraper.py
@@ -1,5 +1,6 @@
 import typing
 from bookie.scrapers import bookmob
+from bookie.scrapers import bookscouter
 from bookie.scrapers import queens
 
 
@@ -7,4 +8,5 @@ def get_resale_prices(isbn: str) -> typing.Dict[str, typing.Optional[str]]:
     return {
         'BookMob': bookmob.get_resale_price(isbn),
         "Queen's": queens.get_resale_price(isbn),
+        'BookScouter': bookscouter.get_resale_price(isbn),
     }
